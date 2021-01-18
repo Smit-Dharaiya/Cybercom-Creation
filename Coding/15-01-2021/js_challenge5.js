0d0ce0700e531d0ddaa385b42b9f1515f4d1d9fb
Smit-Dharaiya <58761900+Smit-Dharaiya@users.noreply.github.com>
2021-01-18 12:00:44 +0530

    15th Jan

---
+++ Coding/15-01-2021/js_challenge5.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $(".wlcm").hover(function(){
+        alert("Welcome to my WebPage !!!");
+    })
+})
---
+++ Coding/15-01-2021/js_challenge6.js
@@ -0,0 +1,11 @@
+console.log("\nFibonaci Series");
+var a=0,b=1,term=4,c,i;
+
+for(i=0;i<term;i++)
+{
+	console.log(a);
+	c=a+b;
+	a=b;
+	b=c;
+}
+
